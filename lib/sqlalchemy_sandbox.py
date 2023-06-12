e272761745eed91453a009148040c548a3a48f24
LimisiMike <michaellimisi7@gmail.com>
2023-06-13 01:48:44 +0300

    Was a success

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -3,10 +3,16 @@
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 
+
+Base = declarative_base()
+# The Base and declarative _base object allow us to avoid rewriting code
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+    
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
